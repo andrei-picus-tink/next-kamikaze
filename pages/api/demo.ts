7413a9ab575b4a577fdff273ba2c2e87bf34b2c0
Andrei Picus <andrei.picus@tink.se>
2019-11-19 11:20:26 +0100

    Fetch titles from Firebase

--- pages/api/demo.ts
+++ pages/api/demo.ts
@@ -1,22 +1,39 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import { NextApiRequest, NextApiResponse } from 'next';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAp67lk3PS_EQ_DBedw7nXt3ENMLINR0sk',
+  authDomain: 'kamikaze-8e410.firebaseapp.com',
+  databaseURL: 'https://kamikaze-8e410.firebaseio.com',
+  projectId: 'kamikaze-8e410',
+  storageBucket: 'kamikaze-8e410.appspot.com',
+  messagingSenderId: '981353420741',
+  appId: '1:981353420741:web:78fa1f85d28a3d862f9dac'
+};
+
+// Prevent app/duplicate-app errors when hot reloading.
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const titlesDoc = firebase.firestore().collection('kamikaze').doc('title');
 
 export interface Demo {
   title: string
 }
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const titles = [
-    'Kamikaze time',
-    'Kaizen time',
-    'Chimichanga time',
-    'Kangaroo time',
-    'Karate time',
-    'Karma time',
-    'Kaput time'
-  ];
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const titleData = (await titlesDoc.get()).data();
+
+  if (!titleData) {
+    return res.status(404);
+  }
+
+  const { titles } = titleData as { titles: string[] };
 
   const index = Math.round(Math.random() * (titles.length - 1));
   const response: Demo = { title: titles[index] };
 
-  res.status(200).json(response);
+  return res.status(200).json(response);
 };
